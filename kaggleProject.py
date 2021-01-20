c89270473f08e775f5a1a99ebcc69d46a0ac0628
caichenghao1991 <caichenghao1991@gmail.com>
2021-01-19 23:12:23 -0600

    update kaggle

--- kaggleProject.py
+++ kaggleProject.py
@@ -6,7 +6,7 @@ import shutil
 import stat
 import subprocess
 import zipfile
-from pathlib import Path
+
 
 from util import get_path
 
@@ -22,31 +22,27 @@ def cmd_execute(com):
 
 def set_kaggle_token():
     sys = platform.system()
-    cur = os.getcwd() + get_path('\\kaggle.json')
+    cur = os.getcwd().replace("\\",'/')+ '/kaggle.json'
     if sys == 'Windows':
-        path = f'C:\\Users\\{getpass.getuser()}\\.kaggle\\kaggle.json'
-        new_dir = '\\'.join(path.split('\\')[:-1])
-        if not Path(new_dir).exists():
-            os.mkdir(new_dir)
-        if not Path(path).exists():
-            shutil.copy(cur, new_dir)
-            os.chmod(path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)  # 777
+        path = 'C:/Users/'+getpass.getuser()+'/.kaggle/kaggle.json'
+        new_dir = '/'.join(path.split('/')[:-1])
     else:
         path = '~/.kaggle/kaggle.json'
         new_dir = '/'.join(path.split('/')[:-1])
-        if not Path(new_dir).exists():
-            os.mkdir(new_dir)
-        if not Path(path).exists():
-            shutil.copy(cur, new_dir)
-            os.chmod(path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+
+    if not os.path.exists(new_dir):
+        os.makedirs(new_dir)
+    if not os.path.exists(path):
+        shutil.copy(cur, new_dir)
+        os.chmod(path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)  # 777
     print(path)
 
-
+'''
 def unzip(count, path=get_path(f'{os.getcwd()}\\static\\data'),flag=True):
     files = []
     suffix = ['rar', 'zip', '7z']
-    lists = os.listdir(path)  # 列出目录的下所有文件和文件夹保存到lists
-    lists.sort(key=lambda fn: os.path.getmtime(path + get_path("\\") + fn), reverse=True)  # 按时间排序
+    lists = os.listdir(path)  # 
+    lists.sort(key=lambda fn: os.path.getmtime(path + get_path("\\") + fn), reverse=True)  # sort by time modified
     res = [x for x in lists[:count] if str(x).split('.')[-1] in suffix]
 
     for file in res:
@@ -64,10 +60,11 @@ def unzip(count, path=get_path(f'{os.getcwd()}\\static\\data'),flag=True):
             if flag:
                 os.remove(file_path)
     return files
-
+'''
 def grab_search_result(topic, count=1):
     cmd = cmd_execute(f'kaggle datasets list -s {topic}')
-    lines = cmd.split('\n')  # 读文件kaggle datasets list -s {topic}
+    #cmd = os.popen(f'kaggle datasets list -s {topic}')
+    lines = cmd.split('\n')  # kaggle datasets list -s {topic}
     column_name = list(filter(lambda x: x is not '', [x for x in lines[0].split('  ')]))
     file_list = [list(filter(lambda x: x is not '', [x for x in lines[i].split('  ')])) for i in range(2, len(lines))]
 
@@ -81,12 +78,17 @@ def grab_search_result(topic, count=1):
     for i in range(count):
         cmd_execute(f'kaggle datasets download {data[i]["ref"]} -p ./static/data')
 
-
+'''
 def retrieve_csv(subject, count):
     if type(count) is type(1) and int(count) < 20:
+        set_kaggle_token()
         grab_search_result(subject, count)
         files = unzip(count)
         logging.warning('all files downloaded and unzipped!')
     return files
+'''
+
 if __name__ == '__main__':  # Runs main() if file wasn't imported.kaggle datasets list - s iris
-    retrieve_csv('iris', 3)
+    #retrieve_csv('iris', 3)
+    #set_kaggle_token()
+    grab_search_result('iris', 3)
